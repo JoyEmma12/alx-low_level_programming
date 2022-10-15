f4b4c88d71238b24e723f7d497a50d293cb8025d
JoyEmma12 <ejoy0600@gmail.com>
2022-10-15 15:42:52 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
-#include <stdio.h>
+#include  <stdio.h>
+
+
+
 /**
- * main - print the phrase "Programming is like a building
+ * main - print the phrase"Programming is like a building
  * a multilingual puzzle
  * Return: 0 if exited properly, non-zero otherwise
  */
@@ -8,4 +11,4 @@ int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-}	
+}
