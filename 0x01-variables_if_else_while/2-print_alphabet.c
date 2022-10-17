e9d9e4123255079d6c6375a157412dd55f5d0862
JoyEmma12 <ejoy0600@gmail.com>
2022-10-17 16:33:33 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: return 0 (success)
+ */
+int main(void)
+{
+char ch = 'a';
+putchar("Lower case alphabet");
+while (ch <= z)
+{
+putchar("%c", ch);
+ch++;
+}
+putchar("\n");
+
+return (0);
+}
