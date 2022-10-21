25780224f845fdb56a66cd6fa1db211da41a503d
JoyEmma12 <ejoy0600@gmail.com>
2022-10-20 17:27:52 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - sum two values
+ * @x: The first number to be checked
+ * @y: The second number to be checked
+ *
+ * Return: Integer value
+ */
+int add(int x, int y)
+{
+return (x + y);
+}
