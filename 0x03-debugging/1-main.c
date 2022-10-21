8edfeb31168074e71cf79eb79746929c6a9da710
JoyEmma12 <ejoy0600@gmail.com>
2022-10-20 19:01:23 -0700

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,7 +7,6 @@
 int main(void)
 {
 int i;
-
 printf("Infinite loop incoming :(\n");
 
 i = 0;
@@ -16,7 +15,6 @@ i = 0;
 /*{*/
 /*putchar(i);*/
 /*}*/
-
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
