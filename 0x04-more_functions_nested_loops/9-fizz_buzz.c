594cb91751d89bacf3b8a6681cebcf0b11dbf4c0
JoyEmma12 <ejoy0600@gmail.com>
2022-10-30 12:10:03 -0700

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * main - Prints fizzbuzz program
+  *
+  * Return: Always 0 (success)
+  */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 100; x++)
+	{
+		if (x % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if ((x % 3 == 0) || (x % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", x);
+		}
+	}
+	printf("\n");
+	return (0);
+}
