73885d7cc55693c91be81f7d6f9d4096e15abd60
JoyEmma12 <ejoy0600@gmail.com>
2022-11-12 13:02:34 -0800

    task 7

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * is_palindrome - return if a string is palindrome
+  * @s: the string to be checked
+  *
+  * Return: integer value
+  */
+int is_palindrome(char *s)
+{
+	if (*s == '0')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (is_palindrome(s));
+}
