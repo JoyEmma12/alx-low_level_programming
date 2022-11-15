6119bde4536489c95426e658882fd42f340bb08d
JoyEmma12 <ejoy0600@gmail.com>
2022-11-15 14:56:19 -0800

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * main - prints all argurments received
+  * @argc: arguements count
+  * @argv: arguements vector
+  *
+  * Return: Always zero
+  */
+int main(int argc, char *argv[])
+{
+	int i;
+	
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
