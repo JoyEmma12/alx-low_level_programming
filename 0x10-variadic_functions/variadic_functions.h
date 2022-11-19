67ceba094d7d26fb39337a8d6456b065dde01d58
JoyEmma12 <ejoy0600@gmail.com>
2022-11-19 12:54:30 -0800

    task 0

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,17 @@
+#ifndef _HEADER_
+#define _HEADER_
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+
+/**
+  * struct format_types - Struct format_types
+  * @identifier: The conversion specifier
+  * @f: The function pointer
+  */
+typedef struct format_types
+{
+	char *identifier;
+	void (*f)(char *seperator, va_list args);
+} f_dt;
+#endif
