4b2717a24b33f93114fcfb538e17243bdf8b58b1
JoyEmma12 <ejoy0600@gmail.com>
2022-11-19 14:06:50 -0800

    task 1

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,6 +9,7 @@ void print_a_char(char *separator, va_list args);
 void print_a_integer(char *separator, va_list args);
 void print_a_float(char *separator, va_list args);
 void print_a_char_ptr(char *separator, va_list args);
+void print_strings(const char *separator, const unsigned int n, ...);
 
 /**
   * struct format_types - Struct format_types
