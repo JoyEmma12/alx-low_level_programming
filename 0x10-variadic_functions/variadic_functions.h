872d063bfbf79003141fe9ba4d215b66e2f8545d
JoyEmma12 <ejoy0600@gmail.com>
2022-11-19 13:28:00 -0800

    task 1

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,7 +4,7 @@
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
-
+int _putchar(char c);
 /**
   * struct format_types - Struct format_types
   * @identifier: The conversion specifier
