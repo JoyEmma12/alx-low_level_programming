3f7db5c1191ec8071808a770ad5317fefd150430
JoyEmma12 <ejoy0600@gmail.com>
2022-11-29 12:30:08 -0800

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  * main - entry point
+  *
+  * Return: Nothing
+  */
+int main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
