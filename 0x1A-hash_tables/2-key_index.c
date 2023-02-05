24eacecc033b9f9dc425969abded02011487b0ef
JoyEmma12 <ejoy0600@gmail.com>
2023-02-05 07:06:36 -0800

    hash tables

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -5,6 +5,8 @@
  * pair should be stored in array of a hash table.
  * @key: The key to get the index of.
  * @size: The size of the array of the hash table.
+ *
+ * Return: The index of a key in a hash table
  */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
